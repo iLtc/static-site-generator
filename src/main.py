83ca1767734e0282127bc1306bb2b18c1b6995e7
Alan Luo <i@iltc.io>
2025-12-30 01:09:33 -0800

    feat: finish copy_static_files

--- src/main.py
+++ src/main.py
@@ -1,9 +1,32 @@
+import os
+import shutil
+
 from textnode import TextType, TextNode
 
 
+def copy_static_files():
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+
+    os.makedirs("public")
+
+    queue = ["static"]
+
+    while queue:
+        current_dir = queue.pop(0)
+        for file in os.listdir(current_dir):
+            if os.path.isdir(os.path.join(current_dir, file)):
+                queue.append(os.path.join(current_dir, file))
+            else:
+                target_dir = os.path.join("public", current_dir.replace("static", "").lstrip("/"))
+
+                if not os.path.exists(target_dir):
+                    os.makedirs(target_dir)
+
+                shutil.copy(os.path.join(current_dir, file), os.path.join(target_dir, file))
+
 def main():
-    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(text_node)
+    copy_static_files()
 
 
 if __name__ == "__main__":
